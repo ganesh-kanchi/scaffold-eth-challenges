89be94af9130b239b3473e90d0d80dfb53594fc0
ganesh <ganeshkanchimad@gmail.com>
2022-08-31 10:01:12 +0530

    feat: token vendor completed

--- packages/hardhat/contracts/Vendor.sol
+++ packages/hardhat/contracts/Vendor.sol
@@ -2,22 +2,47 @@ pragma solidity 0.8.4;
 // SPDX-License-Identifier: MIT
 
 import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/security/ReentrancyGuard.sol";
 import "./YourToken.sol";
 
 contract Vendor is Ownable {
+    //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    event SellTokens(
+        address seller,
+        uint256 amountOfETH,
+        uint256 amountOfTokens
+    );
 
-  //event BuyTokens(address buyer, uint256 amountOfETH, uint256 amountOfTokens);
+    YourToken public yourToken;
 
-  YourToken public yourToken;
+    constructor(address tokenAddress) {
+        yourToken = YourToken(tokenAddress);
+    }
 
-  constructor(address tokenAddress) {
-    yourToken = YourToken(tokenAddress);
-  }
+    uint256 public constant tokensPerEth = 100;
 
-  // ToDo: create a payable buyTokens() function:
+    // ToDo: create a payable buyTokens() function:
+    function buyTokens() public payable {
+        uint256 tokensToDispense = msg.value * tokensPerEth;
+        yourToken.transfer(msg.sender, tokensToDispense);
+        emit BuyTokens(msg.sender, msg.value, tokensToDispense);
+    }
 
-  // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    // ToDo: create a withdraw() function that lets the owner withdraw ETH
+    function withdraw() public onlyOwner {
+        (bool success, ) = payable(address(owner())).call{
+            value: address(this).balance
+        }("");
+        require(success, "Transfer failed.");
+    }
 
-  // ToDo: create a sellTokens(uint256 _amount) function:
-
+    // ToDo: create a sellTokens(uint256 _amount) function:
+    function sellTokens(uint256 tokenAmount) public {
+        uint256 etherAmount = tokenAmount / tokensPerEth;
+        yourToken.transferFrom(msg.sender, address(this), tokenAmount);
+        (bool success, ) = payable(msg.sender).call{value: etherAmount}("");
+        require(success, "Transfer Failed.");
+        emit SellTokens(msg.sender, etherAmount, tokenAmount);
+    }
 }
--- packages/react-app/src/constants.js
+++ packages/react-app/src/constants.js
@@ -1,8 +1,8 @@
 //ToDo: MY INFURA_ID, SWAP IN YOURS FROM https://infura.io/dashboard/ethereum
-export const INFURA_ID = "460f40a260564ac4a4f4b3fffb032dad";
+export const INFURA_ID = "dfe86eacdc1440569f3e048dbf740e96";
 
 // MY ETHERSCAN_ID, SWAP IN YOURS FROM https://etherscan.io/myapikey
-export const ETHERSCAN_KEY = "PSW8C433Q667DVEX5BCRMGNAH9FSGFZ7Q8";
+export const ETHERSCAN_KEY = "IBUZJS5D3C2QR4EJ3NYUWYXGPYXA3JA3ZF";
 
 // BLOCKNATIVE ID FOR Notify.js:
 export const BLOCKNATIVE_DAPPID = "0b58206a-f3c0-4701-a62f-73c7243e8c77";
